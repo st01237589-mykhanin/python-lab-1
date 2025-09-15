23eae365c8f713032aeac343ee927c2893a4161a
Maksym Khanin <st01237589@stud.duikt.edu.ua>
2025-09-15 20:42:34 +0300

    Add check_case method

--- src/string_utils.py
+++ src/string_utils.py
@@ -3,3 +3,15 @@ def print_string(text):
         raise TypeError("Аргумент має бути рядком")
     print(text)
 
+def check_case(text):
+    if not isinstance(text, str):
+        raise TypeError("Аргумент має бути рядком")
+    if not text:
+        return "Порожній рядок"
+    if text.isupper():
+        return "Усі літери великі"
+    elif text.islower():
+        return "Усі літери малі"
+    else:
+        return "Літери змішані"
+
